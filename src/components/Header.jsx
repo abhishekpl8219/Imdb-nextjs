3efd9d55cb5fbb0daa9c17b6e0a3602f79d50127
Abhishek Patial. <abhishekpl8219@gmailom>
2024-04-08 14:29:06 +0530

    Added header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Menuitem from './Menuitem'
+import {AiFillHome} from 'react-icons/ai';
+import {BsFillInfoCircleFill} from 'react-icons/bs';
+import Link from 'next/link';
+
+export default function Header() {
+  return (
+    <div className='flex justify-between items-center p-3 mx-auto max-w-6xl'>
+      <div className='flex gap-4'>
+        <Menuitem  title='home' address='/' Icon={AiFillHome}/>
+        <Menuitem  title='about' address='/about' Icon={BsFillInfoCircleFill}/>
+      </div>
+      <Link href = "/" className='flex items-center gap-2' >
+      <span className='text-2xl font-bold bg-amber-500 rounded-lg py-1 px-2'>Imdb</span>
+      <span className='text-xl hidden  sm:inline'>Clone</span>
+      </Link>
+
+    </div>
+  )
+}
