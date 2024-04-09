494a7ff7aa4ba5f1b502c2a74ed52ee78afe09ff
Abhishek Patial. <abhishekpl8219@gmailom>
2024-04-09 10:37:18 +0530

    Create the about page

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -5,7 +5,7 @@ import {ThemeProvider} from 'next-themes';
 export default function Providers({children}) {
   return (
     <ThemeProvider defaultTheme='system' attribute ='class'>
-    <div>
+    <div >
       {children}
     </div>
     </ThemeProvider>
