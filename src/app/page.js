208bd855810fbaebde41d5c8d1f830f2514e1e36
abhishek <abhishekpl8219@gmail.com>
2024-04-09 13:59:51 +0530

    Add the home components

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,24 @@
-import Image from "next/image";
 
-export default function Home() {
+
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || 'fetchTrending';
+  const res = await fetch(
+    `https://api.themoviedb.org/3${
+      genre === 'fetchTopRated' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=acbd8fc66e247c671d4773387bd60b5c&language=en-US&page=1`
+  );
+  const data = await res.json();
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const results = data.results;
+  console.log(results)
+
   return (
-   <div>
-    home page
-   </div>
+    <div>
+      
+      home
+    </div>
   );
 }
