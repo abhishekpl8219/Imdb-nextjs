f034e8a519f241e7140fcadd340957bf2a14a64d
abhishek <abhishekpl8219@gmail.com>
2024-04-09 15:18:55 +0530

    added some utils in home page

---
+++ src/components/Results.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Results({results}) {
+  return (
+    <div>
+      {results.map((item)=>(
+        <div key = {item.id}>
+           <h2>{item.original_title}</h2>
+            </div>
+      ))}
+    </div>
+  )
+}
