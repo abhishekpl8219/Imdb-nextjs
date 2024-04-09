03c02cba1d575246257e12609092b4fbdcb08b3d
Abhishek Patial. <abhishekpl8219@gmailom>
2024-04-09 10:18:03 +0530

    Added the dark and light mode

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import {MdLightMode,MdDarkMode} from  'react-icons/md';
+import {useTheme} from 'next-themes';
+import { useState,useEffect } from 'react';
+
+export default function DarkModeSwitch() {
+    const {theme,setTheme,systemTheme}=useTheme();
+    console.log("value of theme",theme);
+    console.log("value of systemtheme",systemTheme);
+    const[mounted,setMounted]=useState(false);
+    
+    const currentTheme = theme ==='system' ? systemTheme:theme;
+  return (
+
+    <div>
+       {  (currentTheme ==='dark'?(<MdLightMode onClick={()=>setTheme('light') } className='text-2xl cursor-pointer hover:text-amber-600' ></MdLightMode>):(<MdDarkMode onClick={()=>setTheme('dark')} className='text-2xl cursor-pointer hover:text-amber-600' ></MdDarkMode>)) }
+      
+    </div>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import Menuitem from './Menuitem'
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillInfoCircleFill} from 'react-icons/bs';
 import Link from 'next/link';
+import DarkModeSwitch from './DarkModeSwitch';
 
 export default function Header() {
   return (
@@ -11,10 +12,13 @@ export default function Header() {
         <Menuitem  title='home' address='/' Icon={AiFillHome}/>
         <Menuitem  title='about' address='/about' Icon={BsFillInfoCircleFill}/>
       </div>
+      <div className='flex gap-4 items-center'>
+        <DarkModeSwitch />
       <Link href = "/" className='flex items-center gap-2' >
       <span className='text-2xl font-bold bg-amber-500 rounded-lg py-1 px-2'>Imdb</span>
       <span className='text-xl hidden  sm:inline'>Clone</span>
       </Link>
+      </div>
 
     </div>
   )
