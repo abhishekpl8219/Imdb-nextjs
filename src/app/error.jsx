fb9d01a6db62d93c86f0a51271bb125ef06fa017
abhishek <abhishekpl8219@gmail.com>
2024-04-10 12:00:33 +0530

    add susoense boundaries.

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,13 +1,13 @@
 'use client'
 import  { useEffect } from 'react'
 
-export default function Error({error,reset}) {
+export default function error ({error,reset}) {
     useEffect(()=>{
         console.log(error);
     },[error])
   return (
     <div className=' text-center mt-10'>
-      
+      <h2>fs</h2>
       <h1>Something went wrong.Please try again.</h1>
       <button className='hover:text-amber-600' onClick={()=>reset()}>Try Again</button>
     </div>
--- src/components/NavbarItem.jsx
+++ src/components/NavbarItem.jsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react'
 export default function NavbarItem({title,param}) {
     const searchParams=useSearchParams();
     const genre=searchParams.get('genre')
